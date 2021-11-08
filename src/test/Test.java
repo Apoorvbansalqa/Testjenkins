80a2058c0484c9cf55e810cf060b4e8f4f534ba4
apoor <apoor@LAPTOP-OLHP0GG8>
2021-11-08 12:29:16 +0530

    added hello test

---
+++ src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Test {
+	
+	public static void main(String[] args) {
+	
+		for(int i=1; i<5; i++) {
+			System.out.println("Hello World Apoorv... "+i);
+		}
+			
+		
+	}
+
+}
